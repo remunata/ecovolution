150ae2217d13d7dd13817a7c169b27d124226d61
Mahendra Dinata <remdinata@gmail.com>
2023-12-14 15:19:44 +0700

    refactor

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -1,27 +1,28 @@
 import firebase from "../firebase.js";
 import Favorite from "../models/favoriteModel.js";
-import { getFirestore, collection, doc, addDoc, getDoc, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  getDoc,
+  getDocs,
+} from "firebase/firestore";
 
 const db = getFirestore(firebase);
 
 export const addFavorite = async (req, res, next) => {
   try {
     const data = req.body;
-    console.log(data);
-    console.log(req);
+
+    if (!data.userId || !data.houseId) {
+      return res.status(400).send("User Id and House Id are required");
+    }
+
     const docRef = await addDoc(collection(db, "favorites"), {
       userId: data.userId,
       houseId: data.houseId,
     });
-
-    if (!userId || !houseId) {
-      return res.status(400).send("User Id and House Id are required");
-    }
-
-    const isAlreadyFavourite = db.some((favorite) => favorite.userId === userId && favorite.houseId === houseId);
-    if (isAlreadyFavourite) {
-      return res.status(400).send("House is already in favorites");
-    }
 
     res.status(200).json({
       message: "Favorite added successfully",
@@ -45,25 +46,10 @@ export const getAllFavorites = async (req, res, next) => {
       res.status(400).send("No favorites found");
     } else {
       favorites.forEach((doc) => {
-        const favorite = new Favorite(doc.id, doc.userId, doc.houseId);
+        const favorite = new Favorite(doc.data().userId, doc.data().houseId);
         favoriteArray.push(favorite);
       });
-      res.status(400).send(favoriteArray);
-    }
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
-};
-
-export const getFavoritebyId = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const favorite = doc(db, "favorites", id);
-    const data = await getDoc(favorite);
-    if (data.exists()) {
-      res.status(200).send(new Favorite(data.id, data.data().userId, data.data().houseId));
-    } else {
-      res.status(400).send("Data not found");
+      res.status(200).send(favoriteArray);
     }
   } catch (error) {
     res.status(400).send(error.message);
